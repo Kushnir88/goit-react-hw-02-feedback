e9755a6e99e1e0e391fc1ba20e10b0b6e0d9c894
Igor Kushnir <mr.kushnir.igor@gmail.com>
2023-07-04 12:14:45 +0300

    Update Statistics.jsx

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div>
-      <h2>Feedback Statistics</h2>
+      {/* <h2>Feedback Statistics</h2> */}
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
