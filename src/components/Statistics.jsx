194b6b5c3038d590a060163cc79e446f9dda7e75
Igor Kushnir <mr.kushnir.igor@gmail.com>
2023-07-04 11:58:20 +0300

    на перевірку

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <div>
+      <h2>Feedback Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total Feedback: {total}</p>
+      <p>Positive Feedback Percentage: {positivePercentage}%</p>
+    </div>
+  );
+};
+
+export default Statistics;
